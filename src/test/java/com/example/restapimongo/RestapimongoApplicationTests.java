782cfc79635c99b25a5c45129825a50dfacea9e8
DESKTOP-738ODFT\Gama <gama>
2024-04-26 01:17:45 -0600

    primer commit

---
+++ src/test/java/com/example/restapimongo/RestapimongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapimongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapimongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
